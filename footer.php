67578eaae00bee25728ca5734cbd7fe6f0255a18
Tiwari397978 <tiwariomkar1989@gmail.com>
2022-01-14 16:14:22 +0530

    yes

---
+++ footer.php
@@ -0,0 +1,84 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>Krishi Kisan</h3>
+            <p>
+              Satna<br>
+              Madhay Pradesh, India <br><br>
+              <strong>Phone:</strong> +91-7339896427<br>
+              <strong>Email:</strong> tiwariomkar1989@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="aboutus.php">About</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Staff Login</h4>
+            <p>This feature is Available only for Staff or Administrator</p>
+            <button class="btn btn-success" type="submit" onclick="window.location='adminloginpanel.php';">Click here to Login</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        
+        <div class="credits">
+          Developed By Omkar 
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        
+        <a href="https://www.twitter.com/@OMKARTI22520291" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.facebook.com/omkar.tiwari.79677471" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/omkartiwari93/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="skype:live:.cid.1b5c6f5cd13b3351?chat" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  
+  <script src="assets/js/jquery.dataTables.min.js"></script>
+  <link href="assets/js/dataTables.responsive.min.js" rel="stylesheet">
+  
+<script>
+$(document).ready( function () {
+    $('#datatable').DataTable();
+	
+} );
+</script>
+</body>
+
+</html>
